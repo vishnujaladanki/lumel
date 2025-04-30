5a62bc93e5d16d5fae211207009017d5e15c5b29
Jaladanki <vishnu.jaladanki@hp.com>
2025-04-30 14:56:10 +0530

    hostname incorrect in database string

--- database/db.go
+++ database/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Open() (*gorm.DB, error) {
-	dsn := "host=postgres user=postgres password=admin dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=localhost user=postgres password=admin dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
